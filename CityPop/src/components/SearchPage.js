5f12852aed329702accd1ce92b174a544895b67e
tobiashjalmarsson <tobhja@student.chalmers.se>
2021-02-19 10:17:52 +0100

    (Bug) SearchPage, (Feat) Conditional SearchPage
    -(Bug): Preventing url change when form is submitting to avoid breaking the app.
    
    -(Feat): Adding  conditional rendering of the SearhPage to display if we are searching for a city or a country

--- CityPop/src/components/SearchPage.js
+++ CityPop/src/components/SearchPage.js
@@ -4,19 +4,25 @@ import Header from './Header';
 class SearchPage extends React.Component {
     constructor(props) {
       super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
       this.state = {
         byCity: this.props.location.state.byCity
       }
-    }  
+    }
+    
+    handleSubmit = (e) => {
+      e.preventDefault(); //prevent
+      let searchFor = e.target.elements.search.value;
+      console.log(searchFor);
+    }
 
     render() {
-      console.log(this.state.byCity);
       return(
           <div className="container">
-            <Header subtitle="SEARCH BY CITY"/>
-            <form>
+            {this.state.byCity? <Header subtitle="SEARCH BY CITY"/> : <Header subtitle="SEARCH BY COUNTRY"/>}
+            <form onSubmit={this.handleSubmit}>
               <div>
-                <input type="text" className="search__input"></input>
+                <input type="text" className="search__input" name="search"></input>
               </div>
               <div>
                 <button className="search__button"></button>
