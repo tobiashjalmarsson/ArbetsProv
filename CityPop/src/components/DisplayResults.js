63a10723cf8ad0e0b9e30eb945cae7badba7e545
tobiashjalmarsson <tobhja@student.chalmers.se>
2021-02-20 16:25:20 +0100

    (refactoring) DisplayResults.js
    - (refactoring) DisplayResults.js: Changing DisplayResults.js from stateless to stateful component.

--- CityPop/src/components/DisplayResults.js
+++ CityPop/src/components/DisplayResults.js
@@ -3,22 +3,26 @@ import Header from './Header';
 import City from './City';
 import {withRouter} from 'react-router-dom';
 
-const DisplayResults = (props) => {
-    console.log("Printing props");
-    console.log(props);
-    return (
-        <div className="container">
-            <Header />
-            <div>
-            {props.location.state.results.map((result) => (
-                <City 
-                key={result.population}
-                result={result}
-                />
-            ))}
-            </div>    
-        </div>
-    );
+class DisplayResults extends React.Component {
+    
+
+    render() {
+        console.log("Printing props");
+        console.log(props);
+        return (
+            <div className="container">
+                <Header />
+                <div>
+                {props.location.state.results.map((result) => (
+                    <City 
+                    key={result.population}
+                    result={result}
+                    />
+                ))}
+                </div>    
+            </div>
+        );
+}
 }
 
 export default withRouter(DisplayResults);
