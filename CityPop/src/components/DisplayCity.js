841467491b3824bc20f4f78774a22841b8bacfed
tobiashjalmarsson <tobhja@student.chalmers.se>
2021-02-22 09:46:20 +0100

    - (feat) DisplayCity.js, population formatting
    - (feat) DisplayCity.js, adding formatting function so instead of displaying "1523822" we now display "1 523 822" to make it more readable.

--- CityPop/src/components/DisplayCity.js
+++ CityPop/src/components/DisplayCity.js
@@ -8,6 +8,25 @@ const DisplayCity = (props) => {
     - name can be accessed with: props.location.state.result.name
     - population can be accessed with: props.location.state.result.population
     */
+
+    // Function to formate the population from something like 1515017 to 1 515 017 to make it more readable
+   function formatPopulation(population){
+    let popString = "" + population;
+    let newPop = "";
+    let last = 0;
+    for(let i = 1; i <= popString.length; i++){
+        // We are going to add every third number to the new string
+        if(Number.isInteger(i / 3)){
+            // adds the 3 number between the last added numbers and the current index, also adds in a space
+            newPop = newPop.replace(/^/," " + popString.slice(popString.length - i, popString.length - last));
+            // Updates last so we can keep track of which numbers we have added
+            last = i;
+        }
+    }
+    // adds the last numbers that wherent added, if we have 1515017, 1 will be added here.
+    newPop = newPop.replace(/^/,popString.slice(0, popString.length - last));
+    return newPop;
+   }
     return (
         <div>
             <div className="container">
@@ -17,7 +36,7 @@ const DisplayCity = (props) => {
                 <div className="population__container">
                     <p className="population__txt">Population</p>
                     <p className="population__num">
-                    {props.location.state.result.population}
+                    {formatPopulation(props.location.state.result.population)}
                     </p>
                 </div>
             </div>
